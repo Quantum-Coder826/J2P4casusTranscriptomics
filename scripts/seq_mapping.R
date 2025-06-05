734310921e125bcfe4d5cd07a5359c80be3ee880
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-06-05 11:20:07 +0200

    Added nthreads = 12 to all instances of align()

--- scripts/seq_mapping.R
+++ scripts/seq_mapping.R
@@ -11,24 +11,29 @@ buildindex(
 # Ethanol monsters
 align.eth1 <- align(index = "./RefSeqEColi/E_Coli", 
                     readfile1 = "./dataset/SRR8394576_ethanol_12h_1.fasta.gz", 
-                    output_file = "./bams/eth1.BAM")
+                    output_file = "./bams/eth1.BAM",
+                    nthreads = 12)
 
 align.eth2 <- align(index = "./RefSeqEColi/E_Coli", 
                     readfile1 = "./dataset/SRR8394577_ethanol_12h_2.fasta.gz", 
-                    output_file = "./bams/eth2.BAM")
+                    output_file = "./bams/eth2.BAM",
+                    nthreads = 12)
 
 align.eth2 <- align(index = "./RefSeqEColi/E_Coli", 
                     readfile1 = "./dataset/SRR8394578_ethanol_12h_3.fasta.gz",
-                    output_file = "./bams/eth3.BAM")
+                    output_file = "./bams/eth3.BAM",
+                    nthreads = 12)
 
 # Control monsters
 align.ctrl <- align(index = "./RefSeqEColi/E_coli",
                     readfile1 = "./dataset/SRR8394612_control_12h_1.fasta.gz",
-                    output_file = "./bams/ctrl1.BAM")
+                    output_file = "./bams/ctrl1.BAM",
+                    nthreads = 12)
 
 align.ctrl <- align(index = "./RefSeqEColi/E_coli",
                     readfile1 = "./dataset/SRR8394613_control_12h_2.fasta.gz",
-                    output_file = "./bams/ctrl2.BAM")
+                    output_file = "./bams/ctrl2.BAM",
+                    nthreads = 13)
 
 align.ctrl <- align(index = "./RefSeqEColi/E_coli",
                     readfile1 = "./dataset/SRR8394614_control_12h_3.fasta.gz",
