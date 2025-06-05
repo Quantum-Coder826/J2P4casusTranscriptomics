6eb8e86997a36242a43e6aebb3201aee613afa4b
Quantum-Coder826 <b.veldthuis@gmail.com>
2025-06-05 11:15:08 +0200

    Added all seq mapping steps

--- scripts/seq_mapping.R
+++ scripts/seq_mapping.R
@@ -3,15 +3,36 @@ library(Rsamtools)
 
 # Map de E.coli RefSeq
 buildindex(
-  basename = './ref_ecoli/ref_ecoli',
-  reference = './ref_ecoli/GCF_000005845.2_ASM584v2_genomic.fna',
+  basename = './RefSeqEColi/E_Coli',
+  reference = './RefSeqEColi/GCF_000005845.2_ASM584v2_genomic.fna',
   memory = 8000, # use 8gig of ram
   indexSplit = TRUE)
 
 # Ethanol monsters
-align.eth1 <- align(index = "./ref_ecoli/ref_ecoli", 
+align.eth1 <- align(index = "./RefSeqEColi/E_Coli", 
                     readfile1 = "./dataset/SRR8394576_ethanol_12h_1.fasta.gz", 
                     output_file = "./bams/eth1.BAM")
+
+align.eth2 <- align(index = "./RefSeqEColi/E_Coli", 
+                    readfile1 = "./dataset/SRR8394577_ethanol_12h_2.fasta.gz", 
+                    output_file = "./bams/eth2.BAM")
+
+align.eth2 <- align(index = "./RefSeqEColi/E_Coli", 
+                    readfile1 = "./dataset/SRR8394578_ethanol_12h_3.fasta.gz",
+                    output_file = "./bams/eth3.BAM")
+
+# Control monsters
+align.ctrl <- align(index = "./RefSeqEColi/E_coli",
+                    readfile1 = "./dataset/SRR8394612_control_12h_1.fasta.gz",
+                    output_file = "./bams/ctrl1.BAM")
+
+align.ctrl <- align(index = "./RefSeqEColi/E_coli",
+                    readfile1 = "./dataset/SRR8394613_control_12h_2.fasta.gz",
+                    output_file = "./bams/ctrl2.BAM")
+
+align.ctrl <- align(index = "./RefSeqEColi/E_coli",
+                    readfile1 = "./dataset/SRR8394614_control_12h_3.fasta.gz",
+                    output_file = "./bams/ctrl3.BAM")
 
 # Pull de path van alle *.BAM files in ./bams/
 samples <- list.files("./bams", pattern = "\\.BAM$", full.names = TRUE)
