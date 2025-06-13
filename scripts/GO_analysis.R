8b7a9effb321b73128734f55e4cc16991d761a62
QByte <b.veldthuis@gmail.com>
2025-06-13 12:32:18 +0200

    Added Gene Ontology analysis script

--- scripts/GO_analysis.R
+++ scripts/GO_analysis.R
@@ -1,13 +1,29 @@
 library(tidyverse)
+library(dplyr)
 library(readr)
 library(goseq)
 
-resultaten <- read_rds("./results/dds_results.rds")
+resultaten <- read.csv("./results/dds_resultaten.csv", sep = " ")
+head(resultaten)
 
-#supportedOrganisms() %>% filter(str_detect(Genome, "hg")) # we gaan hg19 gebuiken
+#supportedGeneIDs() %>% filter(str_detect(Genome, "hg")) # we gaan hg19 gebuiken
 
+# Go seq wilt een named-vecotr waar de names de gene's zijn en de values bools
+# Die 1 verhoging 0 verlaging representeren.
+sigData <- as.integer(!is.na(resultaten$padj) & resultaten$padj < 0.01 & resultaten$log2FoldChange > 3)
+names(sigData) <- rownames(resultaten)
+head(sigData)
 
-sigData <- as.integer(!is.na(resultaten$padj) & resultaten$padj < 0.01)
-names(sigData) <- resultaten$GeneID
+pwf <- nullp(sigData, "hg19", "geneSymbol")
+goResults <- goseq(pwf, "hg19", "geneSymbol", test.cats=c("GO:BP"))
 
-pwf <- nullp(sigData, "hg19", "ensGene", bias.data = resultaten$medianTxLength)
+goResults %>% 
+  top_n(10, wt=-over_represented_pvalue) %>% 
+  mutate(hitsPerc=numDEInCat*100/numInCat) %>% 
+  ggplot(aes(x=hitsPerc, 
+             y=term, 
+             colour=over_represented_pvalue, 
+             size=numDEInCat)) +
+  geom_point() +
+  expand_limits(x=0) +
+  labs(x="Hits (%)", y="GO term", colour="p value", size="Count")
